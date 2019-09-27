e3b95b547e5e19da02ebf4168815c303fbfe4c2e
Amir Jumaniyazov <amiromayer@gmail.com>
2019-09-27 11:58:08 +0500

    Added @linkdrop/contracts

--- packages/apps/wallet/data/store/saga/tokens/every/check-if-claimed.js
+++ packages/apps/wallet/data/store/saga/tokens/every/check-if-claimed.js
@@ -2,22 +2,39 @@ import { put, select } from 'redux-saga/effects'
 import { ethers } from 'ethers'
 import { factory } from 'app.config.js'
 import { defineNetworkName } from '@linkdrop/commons'
-import LinkdropFactory from 'contracts/LinkdropFactory.json'
+import LinkdropFactory from '@linkdrop/contracts/build/LinkdropFactory'
 
 const generator = function * ({ payload }) {
   try {
     const chainId = yield select(generator.selectors.chainId)
-    yield put({ type: 'USER.SET_READY_TO_CLAIM', payload: { readyToClaim: false } })
+    yield put({
+      type: 'USER.SET_READY_TO_CLAIM',
+      payload: { readyToClaim: false }
+    })
     yield put({ type: 'USER.SET_LOADING', payload: { loading: true } })
     const { linkdropMasterAddress, linkKey, campaignId } = payload
     const networkName = defineNetworkName({ chainId })
     const provider = yield ethers.getDefaultProvider(networkName)
     const linkWallet = yield new ethers.Wallet(linkKey, provider)
     const linkId = yield linkWallet.address
-    const factoryContract = yield new ethers.Contract(factory, LinkdropFactory.abi, provider)
-    const claimed = yield factoryContract.isClaimedLink(linkdropMasterAddress, campaignId, linkId)
-    yield put({ type: 'USER.SET_ALREADY_CLAIMED', payload: { alreadyClaimed: claimed } })
-    yield put({ type: 'USER.SET_READY_TO_CLAIM', payload: { readyToClaim: true } })
+    const factoryContract = yield new ethers.Contract(
+      factory,
+      LinkdropFactory.abi,
+      provider
+    )
+    const claimed = yield factoryContract.isClaimedLink(
+      linkdropMasterAddress,
+      campaignId,
+      linkId
+    )
+    yield put({
+      type: 'USER.SET_ALREADY_CLAIMED',
+      payload: { alreadyClaimed: claimed }
+    })
+    yield put({
+      type: 'USER.SET_READY_TO_CLAIM',
+      payload: { readyToClaim: true }
+    })
     yield put({ type: 'USER.SET_LOADING', payload: { loading: false } })
   } catch (e) {
     console.error(e)
