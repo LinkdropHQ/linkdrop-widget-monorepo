792df7224e7754655656ce98837e2b289934bc2d
Amir Jumaniyazov <amiromayer@gmail.com>
2019-10-15 16:45:46 +0500

    Removed unneccessary files

--- packages/scripts/src/signUp.js
+++ packages/scripts/src/signUp.js
@@ -1,10 +1,8 @@
 import WalletSDK from '../../sdk/src/WalletSDK'
-import { ethers } from 'ethers'
 const walletSDK = new WalletSDK({})
 
 const main = async () => {
-  const res = await walletSDK.signup('asdf', 'password')
-  console.log('res: ', res)
+  console.log(await walletSDK.signup('email', 'password'))
 }
 
 main()
