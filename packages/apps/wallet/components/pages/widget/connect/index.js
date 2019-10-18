d7273add19cefbbc356af155ffe1bffe0d48e7fe
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2019-10-18 06:28:53 +0300

    auto connect

--- packages/apps/wallet/components/pages/widget/connect/index.js
+++ packages/apps/wallet/components/pages/widget/connect/index.js
@@ -7,28 +7,34 @@ import { translate, actions } from 'decorators'
 @actions(({ user: { email, contractAddress } }) => ({ email, contractAddress }))
 @translate('pages.dappConnect')
 class DappConnect extends React.Component {
+  componentDidMount() {
+    console.log("Confirm screen mounted")
+    this.actions().widget.confirm({})
+  }
+  
   render () {
-    const { email } = this.props
-    return <div className={styles.container}>
-      <DappHeader
-        title={this.t('titles.wallet')}
-        onClose={() => this.actions().widget.close()}
-      />
+    //const { email } = this.props
+    return null
+    // return <div className={styles.container}>
+    //   <DappHeader
+    //     title={this.t('titles.wallet')}
+    //     onClose={() => this.actions().widget.close()}
+    //   />
 
-      <div className={styles.content}>
-        <div className={styles.title} dangerouslySetInnerHTML={{ __html: this.t('titles.loggedIn', { email }) }} />
-        <Button
-          className={styles.button}
-          onClick={() => this.actions().widget.confirm({})}
-        >
-          {this.t('buttons.continue')}
-        </Button>
-      </div>
-      <div
-        className={styles.footer}
-        dangerouslySetInnerHTML={{ __html: this.t('texts.dapp') }}
-      />
-    </div>
+    //   <div className={styles.content}>
+    //     <div className={styles.title} dangerouslySetInnerHTML={{ __html: this.t('titles.loggedIn', { email }) }} />
+    //     <Button
+    //       className={styles.button}
+    //       onClick={() => this.actions().widget.confirm({})}
+    //     >
+    //       {this.t('buttons.continue')}
+    //     </Button>
+    //   </div>
+    //   <div
+    //     className={styles.footer}
+    //     dangerouslySetInnerHTML={{ __html: this.t('texts.dapp') }}
+    //   />
+    // </div>
   }
 }
 
