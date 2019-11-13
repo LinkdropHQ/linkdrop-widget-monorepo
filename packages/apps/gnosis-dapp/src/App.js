b7e66f7b86d46785b814784a47c0aefb36fcd79f
spacehaz <spacehaz@gmail.com>
2019-11-14 01:12:35 +0300

    added fix for button

--- packages/apps/gnosis-dapp/src/App.js
+++ packages/apps/gnosis-dapp/src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
   async _initProvider () {
     const urlParams = this._getParamsFromUrl()
     const network = urlParams.network
-    const widgetUrl = urlParams.widgetUrl || process.env.WIDGET_URL || 'http://localhost:9002'
+    const widgetUrl = urlParams.widgetUrl || process.env.REACT_APP_WIDGET_URL || 'http://localhost:9002'
 
     this.widget = new WalletProvider({
       network,
